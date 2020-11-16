bb9a4da9f7e5325ac1d464006f8d4e6929b6fa3d
iKittyCode <gator.syon@gmail.com>
2020-11-16 14:06:58 -0500

    I love commiting file
    Dear everbody i have discovered i don't need to set this.vel to null i can just pu a semicolon

--- walk.js
+++ walk.js
@@ -1,7 +1,7 @@
 class Walk{
 constructor() {
 this.pos = createVector(200, 200)
-this.vel = null
+this.vel;
 }
 update() {
 this.vel = createVector(random(-3, 3), random(-3, 3))
@@ -9,8 +9,8 @@ this.pos.add(this.vel)
 }
 show() {
 noFill()
-strokeWeight(5)
-stroke(0, 240, 100)
+strokeWeight(4)
+stroke(255);
 point(this.pos.x, this.pos.y, 200, 200)
 }
 }

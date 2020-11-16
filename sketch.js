81130842553f0a029ca5ba0e7a3cb8fdda31c251
iKittyCode <gator.syon@gmail.com>
2020-11-16 15:25:03 -0500

    Hello i was just formmating code As of my dad said that is important Your freind , Syoon

--- sketch.js
+++ sketch.js
@@ -1,12 +1,11 @@
-var walk;
-function setup() {
-walk = new Walk()
-createCanvas(400, 400)
-background(0)
-}
-function draw() {
-
-walk.show()
-walk.update()
-}
+	var walk;
+	function setup() {
+	walk = new Walk()
+	createCanvas(400, 400)
+	background(0)
+	}
+	function draw() {
+	walk.show()
+	walk.update()
+	}
 

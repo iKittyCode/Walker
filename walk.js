1186ebb9a4e36cf1e98d770cbd07c7cdd3e5f6f8
iKittyCode <gator.syon@gmail.com>
2020-11-16 20:48:10 -0500

    Hello As you know i love forrmating So i added a few lines Thanks, Syoon

--- walk.js
+++ walk.js
@@ -6,9 +6,11 @@ this.vel;
 update() {
 this.vel = createVector(random(-3, 3), random(-3, 3))
 this.pos.add(this.vel)
+
 }
 show() {
 noFill()
+
 strokeWeight(4)
 stroke(255);
 point(this.pos.x, this.pos.y, 200, 200)
